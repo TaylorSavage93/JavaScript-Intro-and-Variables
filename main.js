2700b8440e69ea78842d521985abd7053aa776b4
Taylor Savage <taylorsavage93@gmail.com>
2022-08-08 12:28:54 -0400

    finished exercise 1-5

---
+++ main.js
@@ -0,0 +1,29 @@
+//Exercise 1 & 2 
+const firstName = "Taylor";
+let lastName = "Savage";
+var age = 24;
+
+console.log('$my name is {firstName} {lastName} and I am {age} years old"')
+
+//Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let inCaseSensitive = true;
+
+console.log('${language} was first released in {createdYear}')
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"
+
+console.log('$That item costs {price} and the size is {selectedSize}')
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
