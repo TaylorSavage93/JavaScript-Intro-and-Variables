264beddd733f24a42722bee1aec77461fd1222fa
Taylor Savage <taylorsavage93@gmail.com>
2022-08-08 14:36:24 -0400

    fixed quotations

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const firstName = "Taylor";
 let lastName = "Savage";
 var age = 24;
 
-console.log('$my name is {firstName} {lastName} and I am {age} years old"')
+console.log(` my name is ${firstName} ${lastName} and I am ${age} years old`)
 
 //Exercise 3
 let language = "JavaScript";
 let createdYear = 1995;
 let inCaseSensitive = true;
 
-console.log('${language} was first released in {createdYear}')
+console.log(`${language} was first released in ${createdYear}`)
 
 //Exercise 4
 let price = 19.99;
@@ -20,7 +20,7 @@ let stock = 0;
 let inStock = false;
 let selectedSize = "M"
 
-console.log('$That item costs {price} and the size is {selectedSize}')
+console.log(`$That item costs ${price} and the size is ${selectedSize}`)
 //Exercise 5
 let title = "Name of the Wind";
 let author = "Patrick Rothfuss";
