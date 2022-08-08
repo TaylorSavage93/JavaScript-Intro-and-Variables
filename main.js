23f9b1f08b6dc59cf70eee8b16acb22ca76586f5
Taylor Savage <taylorsavage93@gmail.com>
2022-08-08 15:04:21 -0400

    added some information

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 const firstName = "Taylor";
 let lastName = "Savage";
 var age = 24;
+let exercise1 = document.getElementById("exercise1");
+exercise1.innerHTML =`${firstName} ${lastName} ${age}`
+let exercise2 = document.getElementById("exercise2");
+exercise2.innerHTML = `${firstName}`
 
 console.log(` my name is ${firstName} ${lastName} and I am ${age} years old`)
 
@@ -9,8 +13,11 @@ console.log(` my name is ${firstName} ${lastName} and I am ${age} years old`)
 let language = "JavaScript";
 let createdYear = 1995;
 let inCaseSensitive = true;
+let exercise3 = document.getElementById("exercise3");
+exercise3.innerHTML = `${language} ${createdYear}`
 
-console.log(`${language} was first released in ${createdYear}`)
+console.log(`${language} was first released in ${createdYear}`);
+
 
 //Exercise 4
 let price = 19.99;
@@ -19,6 +26,8 @@ let salePercentage = 15;
 let stock = 0;
 let inStock = false;
 let selectedSize = "M"
+let exercise4 = document.getElementById("exercise4");
+exercise4.innerHTML = `${price} ${isOnSale} ${salePercentage} ${inStock} ${selectedSize}`
 
 console.log(`$That item costs ${price} and the size is ${selectedSize}`)
 
@@ -28,3 +37,5 @@ let author = "Patrick Rothfuss";
 let pageCount = 722;
 let bookmark = 456;
 let hasRead = true;
+let exercise5 = document.getElementById("exercise5");
+exercise5.innerHTML = `${title} ${author} ${pageCount} ${bookmark} ${hasRead}`
